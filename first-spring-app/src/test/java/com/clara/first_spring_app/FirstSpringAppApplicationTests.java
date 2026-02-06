65bb7634ded10f8daeba3fc51a8c83ac76e8605d
Clarinhaserafim004 <Clarinhaserafim04@gmail.com>
2026-02-06 13:15:43 -0300

    commit

--- first-spring-app/src/test/java/com/clara/first_spring_app/FirstSpringAppApplicationTests.java
+++ first-spring-app/src/test/java/com/clara/first_spring_app/FirstSpringAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.clara.first_spring_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//teste
 @SpringBootTest
 class FirstSpringAppApplicationTests {
 
